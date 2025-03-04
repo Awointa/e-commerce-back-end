b23551ce13011986c63068c1bd229e238040eb54
Awointa <akosawointa@gmail.com>
2025-03-04 09:48:27 +0100

    done

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3001);
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+  });
 }
 bootstrap();
